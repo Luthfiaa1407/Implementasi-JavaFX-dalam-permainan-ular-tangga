7cc4b034f5791ee4257944a81fa67dfa1d3e9a2b
fernando-FTD <84219686+fernando-FTD@users.noreply.github.com>
2024-12-14 16:08:13 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+package projectulartangga;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("SceneUtama.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+                        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("ular.png")));
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
